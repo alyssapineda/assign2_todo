785650e63f27abe3f4be62ee77a6e770fdba37e0
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-09-22 19:03:11 +1200

    added comments to js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 //create addItem function
 function addItem() {
-  const value = document.querySelector('#form').value;    //assign value by getting elements with id form
-  const ul = document.querySelector("#items");          // assign list by getting elements with id items
-  const btn = document.createElement('button');         //creates a button element
-  btn.innerText = ''                              //adds 'delete' text to the button 
-	btn.setAttribute('data-delete','');                   //create an attribute to button element
+  //declares constant accepting elements with an id form as argument
+  const value = document.querySelector('#form').value;
+  //declares constant accepting elements with an id items as argument
+  const ul = document.querySelector("#items");
+  //create button element
+  const btn = document.createElement('button');
+  //create an attribute to button element                        
+	btn.setAttribute('data-delete','');
 
   //if value given is empty show an alert to inform user
   if (value === '') {                             
     alert("Oops, you forgot to write your task!");
     return
   } 
-  
+  // calls checkSpace function to show an alert if user enters spaces as entry
   else if (checkSpace(value)) {
     alert("Oops, you entered a blank task!");
     return
   }
   // creates an li element
   const li = document.createElement("li");              
-  // adds li elements into html  
+  // adds li elements into html
   li.innerHTML = value; 
-  // appends to the undordered list in HTML document                                  
+  // appends button element with li element in HTML document                                  
   li.appendChild(btn);
+  // appends li element in ul element in HTML document  
   ul.appendChild(li);                                  
+  // when user clicks on li element, put a strike through the text
   li.onclick = (entry) => {
     li.classList.toggle("strike");
   }
@@ -31,11 +36,11 @@ function addItem() {
     const li = event.target.parentNode;
     li.remove()
   }
-
+  // erases user's previous entry
   document.querySelector("#form").value = "";            
 }
 
-//function to check if item only has spaces
+//function to check if item only contains spaces
 function checkSpace(str) {
   // call the trim() method on the string. If the string has a length of 0 then the string contains only spaces
   return str.trim().length === 0;
