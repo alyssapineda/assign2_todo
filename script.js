134fa191797d630ac5457c8429c3e585aba39dea
alyssa <110448688+alyssapineda@users.noreply.github.com>
2022-09-29 01:09:02 +1300

    contact input initial

--- script.js
+++ script.js
@@ -37,12 +37,65 @@ function addItem() {
     li.remove()
   }
   // erases user's previous entry
-  document.querySelector("#form").value = "";            
+  document.querySelector("#form").value = "";
+  console.log("Success");            
+}
+
+//create submit function
+function submitItem() {
+  //declares constant accepting elements with an id form as argument
+  const name = document.querySelector('#name').name;
+  const username = document.querySelector('#username').username;
+  const message = document.querySelector('#message').message;
+
+  //if value given is empty show an alert to inform user
+  if (name === '') or (username === '') or (message === '') {                             
+    alert("Oops, you forgot to write your task!");
+    return
+  } 
+  // calls checkSpace function to show an alert if user enters spaces as entry
+  if (checkSpace(name)) or or (username === '') or (message === '') {
+    alert("Oops, you entered a blank information!");
+    return
+  }
+
+  if (checkName(name)) or (checkName(username)) or (checkName(message)) {
+    alert("Oops, you are lacking more information!");
+    return
+  }
+
+  if(name.length > 40) or (username.length > 40) {
+    alert("Your message is over 200 characters long, please shorten it!")
+    return
+  }
+
+  else if(message.length > 200) {
+    alert("Your message is over 200 characters long, please shorten it!")
+    return
+  }
+
+  // erases user's previous entry
+  document.querySelector("#contact").name = "";
+  document.querySelector("#contact").username = "";
+  document.querySelector("#contact").message = "";
+  console.log("Success");     
+  
 }
 
 //function to check if item only contains spaces
 function checkSpace(str) {
   // call the trim() method on the string. If the string has a length of 0 then the string contains only spaces
   return str.trim().length === 0;
-
 }
+
+//function for less than 1 value
+function checkName(str) {
+  // call the trim() method on the string and checks if string has a length of 1
+  return str.trim().length === 1;
+}
+//function for incorrect email
+
+//function for value greater than 40 for both name and email
+
+//function for value greater than 200 for message
+
